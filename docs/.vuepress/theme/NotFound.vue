8474fee05866754dfe0879b6ae0dbb7190e6bc0d
weibin <1292393108@qq.com>
2019-08-09 11:30:40 +0800

    update

--- docs/.vuepress/theme/NotFound.vue
+++ docs/.vuepress/theme/NotFound.vue
@@ -1,21 +1,20 @@
 <template>
   <div class="container notfound-page">
     <div class="content">
-      <h1>抱歉!</h1>
+      <h1>Not Found</h1>
       <p>{{ getMsg() }}</p>
-      <router-link class="backto" to="/">回到首页.</router-link>
+      <router-link to="/">Take me home.</router-link>
     </div>
   </div>
 </template>
 
 <script>
 const msgs = [
-  "您,跑丢了",
-  '我们是怎么来到这里的?',
-  "404",
-  "Looks like we've got some broken links.",
-  '请再次尝试您的请求或联系支持',
-  "你寻找的页面不存在",
+  `There's nothing here.`,
+  `How did we get here?`,
+  `That's a Four-Oh-Four.`,
+  `Looks like we've got some broken links.`,
+  `Please try your request again or contact support.`,
 ]
 
 export default {
@@ -41,7 +40,7 @@ export default {
   margin-bottom: 24px
 
   h1
-    margin: 0
+    margin-bottom: 0
     font-size: 20px
     font-weight: 500
 
@@ -51,11 +50,6 @@ export default {
 
   p
     margin: 0
-    margin-right: 10px
+    margin-right: 10px;
     font-size: 14px
-
-  .backto
-    font-size: 14px
-    text-decoration: none
-    color: #067df7
 </style>
